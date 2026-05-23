015decff03d0f0b1440055570eea9b52bd9393d9
oscarlmjr <oscarlmjr@gmail.com>
2026-05-23 19:33:19 -0300

    secao_13 aula_573_575

--- loja/urls.py
+++ loja/urls.py
@@ -20,6 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('produto.urls')),
+    path('perfil/', include('perfil.urls')),
+    path('pedido/', include('pedido.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
@@ -29,5 +32,8 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls))
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls))
         
     ] + urlpatterns
